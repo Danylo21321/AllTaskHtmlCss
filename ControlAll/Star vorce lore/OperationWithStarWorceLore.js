96761c7ba0c8932cbcd98d25a99cee490ce984de
nykolyshyn danylo <danylo.nykolyshyn@gmail.com>
2022-04-23 14:10:43 +0300

    task 3 i add background-item (sky star wars) and i add new file style, image

--- ControlAll/Star vorce lore/OperationWithStarWorceLore.js
+++ ControlAll/Star vorce lore/OperationWithStarWorceLore.js
@@ -1,11 +1,9 @@
-let data = prompt("enter your age")
+
 function register(){
+    let data = prompt("enter your age")
     while (true){
         if (data > 3){
             alert("you register")
-            break
-        }else if (data === null){
-            register()
             break
         }else {
             register()
